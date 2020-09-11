ef90b7e175edca13b1624c8abdb1cdbdf9db4b6c
akashi92 <akankshagangwar04@gmail.com>
2020-09-11 11:55:22 +0530

    first commit

---
+++ src/Git_CMD/GitCMD_Test.java
@@ -0,0 +1,14 @@
+package Git_CMD;
+
+public class GitCMD_Test {
+	
+	public static void main(String[] args) {
+		String s="akanksha";
+	
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			System.out.print(s.charAt(i));
+		}
+	}
+
+}
